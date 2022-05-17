c4f45248c22ae4ec35411de73f29db7a295bdcc1
Alvin Faiz <softkopi3258@gmail.com>
2022-05-17 10:52:59 +0700

    pusign

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -44,8 +44,10 @@
           <span class="text-xs">Drink</span>
         </nuxt-link>
         <nuxt-link to="/dashboard/pegawai" class="block p-4 text-white hover:bg-sky-300 text-center">
-          <svg class="w-16 h-16" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z" />
+          <svg class="w-16 h-16" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
+            <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+            <path fill-rule="evenodd" d="M5.216 14A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216z"/>
+            <path d="M4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z"/>
           </svg>
           <span class="text-xs">Pegawai</span>
         </nuxt-link>
